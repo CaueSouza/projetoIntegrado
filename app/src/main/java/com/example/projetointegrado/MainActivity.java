6d68ff3ff437778e6c4276a5beed24f51fc743bb
CaueSouza <caue.souzabarros@hotmail.com>
2020-05-09 18:01:17 -0300

    [tela de cadastro]
    Tela implementada
    botao para acessar a tela de cadastro implementado
    botao para voltar a tela inicial implementado

--- app/src/main/java/com/example/projetointegrado/MainActivity.java
+++ app/src/main/java/com/example/projetointegrado/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.projetointegrado;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.projetointegrado.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.signInButton.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, CadastroActivity.class);
+            startActivity(intent);
+        });
     }
 }
